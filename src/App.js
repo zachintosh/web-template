e17bf6b835bf6ac1972d73d933d6bb46af8b494d
Zachary Williams <zlw11063@gmail.com>
2020-06-30 14:30:37 -0600

    Initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      Content
+    </div>
+  );
+}
+
+export default App;
